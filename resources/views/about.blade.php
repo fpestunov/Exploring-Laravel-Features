811d398eb541a367f4b3fb3183de8b319fdaaf60
fpestunov <fpestunov@gmail.com>
2018-06-27 17:00:52 +0400

    Make output in view About using Blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,11 +4,11 @@
     <title>About page</title>
 </head>
 <body>
-<h1>Hello <?= $name?>!</h1>
+<h1>Hello {{ $name }}!</h1>
     <ul>
-        <?php foreach ($tasks as $task): ?>
-            <li><?= $task?></li>
-        <?php endforeach; ?>
+        @foreach ($tasks as $task)
+            <li>{{ $task }}</li>
+        @endforeach
     </ul>
 </body>
 </html>
