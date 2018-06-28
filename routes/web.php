50fb04031680d1cc38e6ec88582bd40a8a1f2f9f
fpestunov <fpestunov@gmail.com>
2018-06-28 05:58:04 +0400

    Import class Task in routes

--- routes/web.php
+++ routes/web.php
@@ -10,30 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/about', function () {
-    // 3
     $name = 'Mikkie';
     $age = 23;
 
     return view('about', compact('name', 'age'));
-
-    // 2
-    // return view('about')->with('name', 'Jane');
-
-    // 1
-    // return view('about', [
-    //     'name' => 'World II'
-    // ]);
 });
 
 Route::get('/tasks', function () {
 
-    $tasks = App\Task::all();
+    $tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 
@@ -41,7 +33,7 @@ Route::get('/tasks', function () {
 
 Route::get('/tasks/{task}', function ($id) {
 
-    $task = App\Task::find($id);
+    $task = Task::find($id);
 
     //dd($task); // from Helpers function
 
